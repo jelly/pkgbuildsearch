ed8b96cf31fe371ce9f4b4e27179b2b3cc43e43e
Jelle van der Waa <jelle@vdwaa.nl>
2019-11-18 16:24:06 +0100

    cleanup unused imports

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,8 @@ use std::fs::File;
 use std::fs;
 use std::io::prelude::*;
 
-
-use tantivy::collector::TopDocs;
-use tantivy::query::QueryParser;
 use tantivy::schema::*;
 use tantivy::Index;
-use tantivy::ReloadPolicy;
 
 
 pub fn path_exists(path: &str) -> bool {
@@ -19,7 +15,6 @@ pub fn path_exists(path: &str) -> bool {
 
 
 fn main() -> tantivy::Result<()> {
-    //let index_path = TempDir::new("tantivy_example_dir")?;
     let index_path = "/tmp/pkgbuildsearch";
     let mut schema_builder = Schema::builder();
 
@@ -28,13 +23,15 @@ fn main() -> tantivy::Result<()> {
 
     let schema = schema_builder.build();
     let index = Index::create_in_dir(&index_path, schema.clone())?;
+
+    // 50 MiB
     let mut index_writer = index.writer(50_000_000)?;
 
     let pkgbase = schema.get_field("pkgbase").unwrap();
     let pkgbuild = schema.get_field("pkgbuild").unwrap();
 
     // configuration variable/option
-    for entry in read_dir("/home/jelle/projects/pkgbuildsearch/packages/")? {
+    for entry in read_dir("/home/jelle/projects/packages/")? {
         let entry = entry?;
         let path = entry.path();
         let basename = entry.path().clone();
@@ -64,23 +61,7 @@ fn main() -> tantivy::Result<()> {
 
     index_writer.commit()?;
 
-    /*
-    let reader = index
-        .reader_builder()
-        .reload_policy(ReloadPolicy::OnCommit)
-        .try_into()?;
-
-    let searcher = reader.searcher();
-    let query_parser = QueryParser::for_index(&index, vec![pkgbase, pkgbuild]);
-
-    let query = query_parser.parse_query("fuck")?;
-
-    let top_docs = searcher.search(&query, &TopDocs::with_limit(10))?;
-    for (_score, doc_address) in top_docs {
-        let retrieved_doc = searcher.doc(doc_address)?;
-        println!("{}", schema.to_json(&retrieved_doc));
-    }
-    */
+    println!("Finished indexing");
 
     Ok(())
 }
